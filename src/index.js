35f0bdcbea71794e0328d1ad79280dd35e86650d
rtexelm <texelist@gmail.com>
2022-11-11 11:07:21 -0500

    Merge to main after some additions

--- src/index.js
+++ src/index.js
@@ -3,4 +3,8 @@ import Example from "./scripts/example";
 document.addEventListener("DOMContentLoaded", () => {
   const main = document.getElementById("main");
   new Example(main);
+
+  fetch('https://callook.info/KD2QOQ/json')
+    .then((response) => response.json())
+    .then((data) => console.log(data));
 });
