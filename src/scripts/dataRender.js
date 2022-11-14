5c0c7c71751b6bee16d3070fdd7af4fa0acc306e
rtexelm <texelist@gmail.com>
2022-11-13 23:01:02 -0500

    Forming array of callsigns

--- src/scripts/dataRender.js
+++ src/scripts/dataRender.js
@@ -1,27 +1,46 @@
 // Handle the list creation by fetching info from the JSON file of zips
+
+import Data from "../assets/NY_callsigns.json"
+
 class DataRender {
   constructor (button, form) {
     this.button = button;
     this.form = form;
     this.form.addEventListener("submit", this.handleClick.bind(this));
-    this.form.addEventListener("formdata", this.handleForm.bind(this));
+    // this.form.addEventListener("formdata", this.handleForm.bind(this));
   }
 
   handleClick(e) {
     e.preventDefault();
     console.log("clicccccked");
-    const formData = new FormData(this.form)
+    const formData = new FormData(this.form);
+    const zipcodeField = document.getElementById("zipcode");
+    const zipcode = zipcodeField.value;
+    zipcodeField.value = "";
+    console.log(zipcode)
+    const callsigns = this.getSigns(Data, zipcode)
+    console.log(callsigns)
     // for (let [k, v] of formData.entries()) {
     //   console.log([k, v]);
     // }
-
   }
 
-  handleForm(e) {
-    console.log("formdata begins");
-    const data = e.formData;
-    console.log(data)
+  getSigns(data, key) {
+    let signs = []
+    let licenses = data[key]
+    Object.keys(licenses).forEach(license => {
+      signs.push(license.cs);
+    })
+    return signs
   }
+
+
+
+  // handleForm(e) {
+  //   console.log("formdata begins");
+  //   const data = e.formData;
+  //   console.log(data);
+  // }
 
 }
 
--- src/scripts/fetcher.js
+++ src/scripts/fetcher.js
@@ -3,7 +3,18 @@ Get a set of data from a key in NY_full_json
 Make a request to the API for one of the callsigns
 Get user input for a Zip Code
 */
+// import Data from "assets/NY_callsigns.json"
 
-// class Fetcher {
-//   constructor (url, options)
-// }
+class Fetcher {
+  constructor (data, key) {
+    this.data = data
+    this.key = key
+  }
+
+  getSigns(data, key) {
+    
+    
+    
+  }
+
+}
