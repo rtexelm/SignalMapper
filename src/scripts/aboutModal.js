9dda5cfb84736d0650a7f2295ff032c89d2662a5
rtexelm <texelist@gmail.com>
2022-11-16 18:55:13 -0500

    Further detailed styling

---
+++ src/scripts/aboutModal.js
@@ -0,0 +1,32 @@
+// Control about page
+class Modal {
+  constructor (trigger) {
+    this.trigger = trigger;
+    this.trigger.addEventListener("click", this.controlModal.bind(this));
+  }
+
+  controlModal (e) {
+    e.preventDefault();
+    const aboutModal = document.getElementById("about")
+    const close = document.getElementById("close")
+    const hideMap = document.getElementById("map")
+
+    aboutModal.style.display = "block";
+    hideMap.style.display = "none";
+
+    close.addEventListener("click", () => {
+      aboutModal.style.display = "none";
+      hideMap.style.display = "block";
+    })
+
+    window.onclick = e => {
+      if (e.target == aboutModal) {
+        aboutModal.style.display = "none";
+        hideMap.style.display = "block";
+      }
+    }
+  }
+
+}
+
+export default Modal
