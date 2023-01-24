0f46a6df044b79061da20bc63a9d7314968bf8d0
rtexelm <texelist@gmail.com>
2023-01-23 21:24:26 -0500

    Clean up files

--- src/scripts/fetcher.js
+++ src/scripts/fetcher.js
@@ -3,10 +3,10 @@ On user click of list item
 Make a request to the API for one of the callsigns
 */
 
-import map from "./map"
+import map from "./map";
 
 class Fetcher {
-  constructor (callsign) {
+  constructor(callsign) {
     // debugger;
     this.callsign = callsign;
     this.csString = this.callsign.innerHTML.toLowerCase();
@@ -14,58 +14,56 @@ class Fetcher {
     this.license;
   }
 
-  getLicenseData (e) {
-    console.log(this.csString)
+  getLicenseData(e) {
+    // console.log(this.csString)
     // let license;
     fetch(`https://callook.info/${this.csString}/json`)
-      .then(res => {
+      .then((res) => {
         if (res.ok) {
           return res.json();
         } else {
           throw res;
         }
       })
-      .then(data => {
+      .then((data) => {
         console.log(data);
         this.setLicense(data);
         this.createDataSection(this.license);
-        let loc = this.snatchCoordz(this.license)
+        let loc = this.snatchCoordz(this.license);
         let marker = L.marker(loc).addTo(map);
         marker.bindPopup(this.csString).openPopup();
         map.flyTo(marker.getLatLng());
       })
-      .catch(error => console.error(error));
-      
+      .catch((error) => console.error(error));
   }
 
-  setLicense (obj) {
+  setLicense(obj) {
     this.license = obj;
     // console.log(this.license.location)
   }
 
-  createDataSection (obj) {
-    let list = document.getElementById("licenseData")
+  createDataSection(obj) {
+    let list = document.getElementById("licenseData");
     list.className = "activated";
     list.innerHTML = "";
     // let dataItem = document.createElement("li");
 
     for (const property in obj) {
-
       let dataItem = document.createElement("li");
       if (obj[property] === "") continue;
       if (obj[property] instanceof Object) {
-        console.log("Unpacking Object")
-        dataItem.innerHTML = `${titleize(property)}: `
-        this.objLister(obj[property], dataItem)
+        console.log("Unpacking Object");
+        dataItem.innerHTML = `${titleize(property)}: `;
+        this.objLister(obj[property], dataItem);
       } else {
         dataItem.innerHTML = `${titleize(property)}: ${obj[property]}`;
-        }
-        
+      }
+
       list.appendChild(dataItem);
     }
   }
 
-  objLister (obj, node) {
+  objLister(obj, node) {
     let subList = document.createElement("ul");
 
     for (const property in obj) {
@@ -77,23 +75,24 @@ class Fetcher {
     node.append(subList);
   }
 
-  snatchCoordz (pojo) {
-    let lat = pojo.location.latitude
-    let long = pojo.location.longitude
-    console.log([lat,long])
-    return [lat, long]
+  snatchCoordz(pojo) {
+    let lat = pojo.location.latitude;
+    let long = pojo.location.longitude;
+    console.log([lat, long]);
+    return [lat, long];
   }
-
 }
 
-function filterData(pojo) {
-
-}
+function filterData(pojo) {}
 
 function titleize(str) {
-  return str.toLowerCase().split(' ').map((word) => {
-    return (word.charAt(0).toUpperCase() + word.slice(1));
-  }).join(' ');
+  return str
+    .toLowerCase()
+    .split(" ")
+    .map((word) => {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    })
+    .join(" ");
 }
 
-export default Fetcher
+export default Fetcher;
